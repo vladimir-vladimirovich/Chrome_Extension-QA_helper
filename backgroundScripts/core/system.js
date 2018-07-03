b1ddf8a9333872e00348c211d81d92939b0c7194
Vladimir <vladimir.ovcharov@playtech.com>
2018-07-03 17:29:26 +0300

    Added configurable link to version.json

--- backgroundScripts/core/system.js
+++ backgroundScripts/core/system.js
@@ -7,17 +7,29 @@ chrome.runtime.onMessage.addListener((request) => {
     if(request.URLChange) {
         environment.defaultEnvironmentURL = request.URLChange;
     }
+
+    if(request.versionPathChange) {
+        environment.defaultFEJSONPass = request.versionPathChange;
+    }
 });
 
 /**
  * Check if there is default URL set in storage and if it is - use it
  */
 export let checkDefaultURL = () => {
-  chrome.storage.local.get(['defaultURL'], function (result) {
-      console.log("checking storage...");
-      if(result.defaultURL) {
-          console.log("result.defaultURL: " + result.defaultURL);
-          environment.defaultEnvironmentURL = result.defaultURL;
-      }
-  })
+    chrome.storage.local.get(['defaultURL'], function (result) {
+        console.log("checking storage...");
+        if(result.defaultURL) {
+            console.log("result.defaultURL: " + result.defaultURL);
+            environment.defaultEnvironmentURL = result.defaultURL;
+        }
+    });
+
+    chrome.storage.local.get(['defaultVersionPath'], function (result) {
+        console.log("checking storage...");
+        if(result.defaultVersionPath) {
+            console.log("result.defaultVersionPath: " + result.defaultVersionPath);
+            environment.defaultFEJSONPass = result.defaultVersionPath;
+        }
+    })
 };
--- popup/popup.js
+++ popup/popup.js
@@ -6,8 +6,11 @@ form.addEventListener('submit', function (element) {
     console.log("New URL: " + element.target.elements['url'].value);
 
     let newURL = element.target.elements['url'].value;
+    let versionPath = element.target.elements['versionPath'].value;
 
     chrome.storage.local.set({defaultURL: newURL});
+    chrome.storage.local.set({defaultVersionPath: versionPath});
 
     chrome.runtime.sendMessage({URLChange: newURL});
+    chrome.runtime.sendMessage({versionPathChange: versionPath});
 });
