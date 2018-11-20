a091eab8952d185bc5a0158dfd004106a18a0c7b
Vladimir <vladimir.ovcharov@playtech.com>
2018-11-20 10:25:05 +0200

    Fixed issue when version path wasn't set

--- backgroundScripts/core/requestHandler.js
+++ backgroundScripts/core/requestHandler.js
@@ -1,4 +1,4 @@
-import { environment } from "../config/projectProperties.js";
+import {environment} from "../config/projectProperties.js";
 
 /**
  * 1. Sends AJAX request
@@ -9,28 +9,35 @@ export default {
     getVersionJSON(callback) {
         let xhr = new XMLHttpRequest();
 
+        console.log('888 environment.defaultEnvironmentURL + environment.defaultFEJSONPass');
+        console.log(environment.defaultEnvironmentURL + environment.defaultFEJSONPass + " ololo");
+
         // Set connection options
         xhr.open('GET', environment.defaultEnvironmentURL + environment.defaultFEJSONPass, true);
 
         // Actions to be performed on load
         xhr.onload = () => {
-            let versionJSON = JSON.parse(xhr.responseText);
+            if (environment.defaultEnvironmentURL === "" && environment.defaultFEJSONPass === "") {
+                callback("Bad XMLHttpResponse... :(");
+            } else {
+                let versionJSON = JSON.parse(xhr.responseText);
 
-            // Delete unnecessary property: commit log
-            delete versionJSON.WPL_Git_Log;
+                // Delete unnecessary property: commit log
+                delete versionJSON.WPL_Git_Log;
 
-            // Stringify parsed json
-            let resultString = '';
-            for(let prop in versionJSON) {
-                resultString = resultString + prop + ": " + versionJSON[prop] +  '\n';
+                // Stringify parsed json
+                let resultString = '';
+                for (let prop in versionJSON) {
+                    resultString = resultString + prop + ": " + versionJSON[prop] + '\n';
+                }
+
+                callback(resultString);
             }
-
-            callback(resultString);
         };
 
         // ERROR handler
-        xhr.onerror = function() {
-            console.log( '[-_-] XMLHttpRequest ERROR: ' + this.status );
+        xhr.onerror = function () {
+            console.log('[-_-] XMLHttpRequest ERROR: ' + this.status);
             callback("Bad XMLHttpResponse... :(");
         };
 
